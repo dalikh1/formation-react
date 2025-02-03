db82da41293ee4df821d1a73df53790f06e666e3
dalikh1 <kheirdindali@gmail.com>
2025-02-03 18:11:23 +0100

    seance1: inception

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * <div id='parent'>
+ *  <div id='child'>
+ *    <h1>mohamed ali</h1>
+ *    <h2>hello</h2>
+ * </div>
+ * </div>
+ */
+const parent = React.createElement(
+  "div",
+  { id: "parent" },[
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "mohamed ali"),
+    React.createElement('h2',{},'hello')
+  )]
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(parent)
+
+// const heading = React.createElement('h1',{id:'heading',name:'dali'},'hello world from react!');
+// console.log(heading) // it is an object
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading)
